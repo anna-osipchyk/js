b46c82504f5db068509ef25547759dc1b963899f
Anna Osipchik <82057714+anna-osipchyk@users.noreply.github.com>
2021-10-20 20:58:16 +0300

    сделала дз 7

---
+++ lesson_7/3 уровень/1-tabs/app.js
@@ -0,0 +1,59 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+let text = document.querySelector('.text');
+let nav_links = document.querySelectorAll('.nav-link');
+nav_links.forEach(nav_link => nav_link.addEventListener('click', clickHandler));
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+function clickHandler(event) {
+    // здесь вызывайте changeText и changeActiveClass, и передавайте
+    // им объект события.
+    changeText(event);
+    changeActiveClass(event);
+
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(event) {
+    document.querySelector('.active').classList.remove('active');
+    event.target.classList.add("active");
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    switch (event.target.innerText) {
+        case "Link 1":
+            text.innerText = texts.text1;
+            break;
+        case "Link 2":
+            text.innerText = texts.text2;
+            break;
+        case "Link 3":
+            text.innerText = texts.text3;
+            break;
+
+    }
+
+}
